3ed0a163532167d89544eed5e9fdb9c1c7a77c9d
Close-your-eyes <vonskopnik@pm.me>
2021-12-30 08:41:38 +0100

    up

--- R/alt_5pl_model.R
+++ R/alt_5pl_model.R
@@ -5,16 +5,22 @@
 #' based on standard values (aa, dd) and on a previous regression made by the bioplex
 #' biomanager (cc, dd, gg).
 #'
+#' Model for fitting is FI ~ dd + (aa - dd) / ((1 + (Conc / cc)^bb))^gg.
+#' dd corresponds to the horizontal asymptote for x -> -Inf,
+#' aa corresponds to the horizontal asymptote for x -> Inf.
+#' (https://laustep.github.io/stlahblog/posts/5pl.html)
+#'
 #' @param list one list entry as derived from plexr::read_bioplex
 #' @param n_iter number of iterations for nls.multstart::nls_multstart
 #' @param weights on which end of the regression does good fitting to standards
 #' is required most (e.g. where do the measured values lie), lower range or upper range
-#' or no weights (none); only used if FI is NULL.
-#' @param FI provide custom weights instead of the easy-to-use weights parameter
+#' or no weights (none); alternatively provide a numeric vector of weights -
+#' has to be same length as list$standard$FI; rows of list$standard will be order increasingly
+#' by FI before fitting; take this into account when providing numeric weights.
 #' @param model_name if NULL, just the model is returned; if model_name is not NULL
 #' the model is added under that name to list and list is returned
 #' @param ... additional parameters passed to nls.multstart::nls_multstart; e.g.
-#' control = nls.lm.control(maxiter = 1024, maxfev=10000); or supp_errors = "Y"
+#' control = minpack.lm::nls.lm.control(maxiter = 1024, maxfev=10000); or supp_errors = "Y"
 #'
 #' @return
 #' @export
@@ -23,14 +29,12 @@
 alt_5pl_model <- function(list,
                           n_iter = 1000,
                           weights = c("lower", "upper", "none"),
-                          FI = NULL,
                           model_name = NULL,
                           ...) {
 
-  weights <- match.arg(weights, c("lower", "upper", "none"))
-
-  #start_lower <- c(aa = max(y), dd = min(y)/1.5, gg = 0.1, bb = -2, cc = 100)
-  #start_upper <- c(aa = 100*max(y), dd = min(y)*1.5, gg = 5, bb = -0.1, cc = 10000)
+  if (!is.numeric(weights)) {
+    weights <- match.arg(weights, c("lower", "upper", "none"))
+  }
 
   start_lower <- c(aa = max(list$standard$FI),
                    dd = min(list$standard$FI)/1.5,
@@ -51,16 +55,21 @@ alt_5pl_model <- function(list,
                    cc = list$std_curve_pars[["cc"]]*100
   )
 
-  # somehow passing data = list$standard to nls_multstart
-  if (is.null(FI)) {
+  list$standard <- list$standard[order(list$standard$FI),]
+
+  if (!is.numeric(weights)) {
     if (weights == "lower") {
-      FI <- list$standard$FI
+      weights <- rev(list$standard$FI)^2
     }
     if (weights == "upper") {
-      FI <- list$standard$FI^2
+      weights <- list$standard$FI^2
     }
     if (weights == "none") {
-      FI <- rep(1, length(list$standard$FI))
+      weights <- rep(1, length(list$standard$FI))
+    }
+  } else {
+    if (length(weights) != length(list$standard$FI)) {
+      stop("weights has to have the same length as list$standard$FI.")
     }
   }
 
@@ -69,7 +78,7 @@ alt_5pl_model <- function(list,
                                         iter = n_iter,
                                         start_lower = start_lower,
                                         start_upper = start_upper,
-                                        modelweights = rev(FI)^2,
+                                        modelweights = weights,
                                         ...)
 
 

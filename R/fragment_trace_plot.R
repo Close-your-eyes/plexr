8c31c53808740fe27a72cc71f55d14887d979732
Close-your-eyes <vonskopnik@pm.me>
2024-01-27 07:22:41 +0100

    up

--- R/fragment_trace_plot.R
+++ R/fragment_trace_plot.R
@@ -14,7 +14,11 @@
 #' @param x_lims x-axis limits
 #' @param facetting how to facet the plot; see example
 #' @param line_args arguments to ggplot2::geom_line
-#' @param text_args arguments to ggrepel::geom_text_repel
+#' @param text_args arguments to ggrepel::geom_text_repel; pass empty list to omit
+#' plotting text annotation; pass a list of lists to plot multiple annotations
+#' @param text_fun which function to use for plotting text annotation;
+#' pass a list of functions for every list element in text_args or one function
+#' for every element in text_args
 #'
 #' @return a ggplot2 plot object
 #' @export
@@ -40,17 +44,42 @@ fragment_trace_plot <- function(FA_data,
                                                   strip.background = ggplot2::element_rect(fill = "cornsilk")),
                                 facetting = NULL,
                                 x_lims = c(5,NA),
-                                text_fun = ggrepel::geom_label_repel,
-                                text_args = list(data = FA_data[[quality_entry]],
-                                                 mapping = ggplot2::aes(label = RQN),
-                                                 x = -Inf,
-                                                 y = Inf,
-                                                 size = 5,
-                                                 min.segment.length = Inf),
+                                text_fun = list(ggrepel::geom_label_repel, ggrepel::geom_text_repel),
+                                text_args = list(list(data = FA_data[[quality_entry]],
+                                                      mapping = ggplot2::aes(label = RQN),
+                                                      x = -Inf,
+                                                      y = Inf,
+                                                      size = 4,
+                                                      min.segment.length = Inf),
+                                                 list(data = FA_data[[quality_entry]],
+                                                      mapping = ggplot2::aes(label = chemical_code),
+                                                      x = -Inf,
+                                                      y = -Inf,
+                                                      size = 3,
+                                                      min.segment.length = Inf)),
                                 line_args = list(color = "tomato2")) {
 
   if (!is.list(FA_data)) {
     stop("FA_data has to be a list.")
+  }
+
+
+  if (is.list(text_args) && any(!sapply(text_args, is.list))) {
+    # this tests if the list is a list of lists
+    # wraps a list around text_args if text_args is an immediate list of text arguments
+    text_args <- list(text_args)
+  }
+
+  if (length(text_fun) < length(text_args) && length(text_fun) != 1) {
+    stop("length of text_fun unequal to length of text_args. provide equal length of both or text_fun of length 1.")
+  }
+
+  if (!is.list(text_fun)) {
+    text_fun <- list(text_fun)
+  }
+
+  if (length(text_fun) < length(text_args)) {
+    text_fun <- rep(text_fun, length(text_args))
   }
 
   if (!electropherogram_entry %in% names(FA_data)) {
@@ -77,9 +106,11 @@ fragment_trace_plot <- function(FA_data,
     ggplot2::scale_x_log10(breaks = x_breaks, limits = x_lims)
 
   if (length(text_args) > 0) {
-    FA_plot <-
-      FA_plot +
-      do.call(text_fun, args = text_args)
+    for (i in seq_along(text_args)) {
+      FA_plot <-
+        FA_plot +
+        do.call(text_fun[[i]], args = text_args[[i]])
+    }
   }
 
   FA_plot <- FA_plot + theme
